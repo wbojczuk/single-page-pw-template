2dc29cc08ed70c78ce1abdd7942744730630348d
William Bojczuk <williambojczuk@gmail.com>
2023-10-04 11:17:04 -0400

    updated estimate form

--- src/components/FreeEstimateForm/FreeEstimateForm.tsx
+++ src/components/FreeEstimateForm/FreeEstimateForm.tsx
@@ -1,14 +1,47 @@
 // import PhoneNumber from "./PhoneNumber/PhoneNumber"
+import { useRef, useState } from "react"
 import "./freeestimateform.css"
 import emailIcon from "./icons/email.svg"
 import locationIcon from "./icons/location.svg"
 import personIcon from "./icons/person.svg"
 import phoneIcon from "./icons/phone.svg"
+import MessageStatus from "../MessageStatus/MessageStatus"
 
 export default function FreeEstimateForm() {
+
+    const [status, setStatus] = useState("none")
+    const formRef: any = useRef();
+
+    async function handleSubmit(evt: any) {
+        evt.preventDefault();
+        const formData = new FormData(evt.currentTarget);
+        const formProps = Object.fromEntries(formData);
+
+        
+        try{
+            setStatus("loading")
+            await fetch("https://formsubmit.co/example@email.com ",{
+                method: "POST",
+                body: JSON.stringify(formProps),
+                headers: {
+                    'Content-Type': 'application/json'
+                  }
+            })
+        }catch(err){
+            console.error(err)
+            setStatus("error")
+        }finally{
+            setStatus("success")
+            formRef.current.reset()
+        }
+        
+      }
+
+
   return (
     <>
-    <form method="POST" action="https://formsubmit.co/97aca8e29358f1aec86ce3da24c5e17f" id="estimateForm">
+    <MessageStatus status={status} setStatus={setStatus}/>
+    <form ref={formRef} id="estimateForm" onSubmit={handleSubmit}>
         {/* <input type="hidden" name="_cc" value="example@gmail.com"/> */}
         <input type="hidden" name="_next" value={`${window.location.origin}?messagesuccess=true${location.hash}`}/>
         <input type="hidden" name="_captcha" value="false"/>
