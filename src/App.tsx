701d48c1e8cee614ed51aff5554b82247a9c0566
William Bojczuk <williambojczuk@gmail.com>
2023-10-04 09:45:43 -0400

    Updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import Reviews from "./components/Reviews/Reviews"
 import Services from "./components/Services/Services"
 import PastWork from "./components/PastWork/PastWork"
 import AboutSection from "./components/AboutSection/AboutSection"
+import Footer from "./components/Footer/Footer"
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+    
+    </>
   )
 }
